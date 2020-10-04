caaf879a7af43d5a4cd026fd167dec242084cb6b
Alina Balysh <lina.balysh@gmail.com>
2020-10-04 13:00:47 +0300

    refactor: add spaces

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let turnsCount=Math.pow(2, disksNumber)-1;
-  let secondsCount = Math.floor(turnsCount*3600/turnsSpeed);
-  return {turns: turnsCount, seconds: secondsCount};
+  let turnsCount = Math.pow(2, disksNumber) - 1;
+  let secondsCount = Math.floor(turnsCount * 3600 / turnsSpeed);
+  return {
+    turns: turnsCount,
+    seconds: secondsCount
+  };
 };
