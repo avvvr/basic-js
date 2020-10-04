fa91c8d9b980d1b8bbe6ba7ec2f364a3d4402209
Alina Balysh <lina.balysh@gmail.com>
2020-10-04 14:50:08 +0300

    feat: add chainMaker function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  array: [],
+  toString() {
+    let resultString;
+    for (let i = 0; i < this.array.length; i++) {
+      if (i === 0) resultString = `(${this.array[i]})`;
+      else resultString += `~~(${this.array[i]})`;
+    }
+    return resultString;
+  },
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) this.array.push(' ');
+    else {
+      this.array.push(` ${String(value)} `);
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (isNaN(position) === true ||
+      this.array[position] === undefined ||
+      Number.isInteger(position) === false) {
+      this.array.splice(0, this.array.length);
+      throw new Error;
+    } else {
+      this.array.splice((position - 1), 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let newArray = [];
+    for (let i = 0; i < this.array.length; i++) {
+      newArray[i] = this.array[this.array.length - 1 - i];
+    }
+    this.array = newArray;
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.toString();
+    this.array.splice(0, this.array.length);
+    return result;
   }
 };
 
